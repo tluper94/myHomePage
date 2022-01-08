abf068d0eedb8a5dad6aaa944fa01a66850c7af9
trevor <luper4@gmail.com>
2022-01-08 00:44:19 -0600

    Added weather api and draggable component

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,5 +1,42 @@
-function Weather() {
-    return <div></div>
+import { useEffect, useState } from 'react';
+
+function Weather(props) {
+     const [weatherData, setWeatherData] = useState();
+
+     console.log(props);
+
+     useEffect(() => {
+          const getData = async () => {
+               const weatherApi =
+                    'http://api.weatherapi.com/v1/forecast.json?key=25bd208b8a974389aea40747220501 &q=76504&days=7&aqi=no&alerts=yes';
+               await fetch(weatherApi)
+                    .then((data) => data.json())
+                    .then((res) => {
+                         setWeatherData(res);
+                    });
+          };
+          getData();
+     }, []);
+
+     console.log(weatherData);
+
+     const displayCurrent = () => {
+          if (weatherData) {
+               return <h1>{weatherData.current.temp_f}F</h1>;
+          }
+     };
+
+     return (
+          <div
+               onTouchEnd={props.onTouchEnd}
+               onMouseUp={props.onMouseUp}
+               onMouseDown={props.onMouseDown}
+               style={props.style}
+               className={`${props.className} box`}
+          >
+               {displayCurrent()}
+          </div>
+     );
 }
 
-export default Weather
+export default Weather;
