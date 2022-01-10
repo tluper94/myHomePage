d3594e63dfb88869226124a44dd624a6e28b9a26
trevor <luper4@gmail.com>
2022-01-10 01:34:27 -0600

    Refined weather component

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -7,7 +7,7 @@ function Weather(props) {
      useEffect(() => {
           const getData = async () => {
                const weatherApi =
-                    'http://api.weatherapi.com/v1/forecast.json?key=25bd208b8a974389aea40747220501 &q=76504&days=7&aqi=no&alerts=yes';
+                    'http://api.weatherapi.com/v1/forecast.json?key=25bd208b8a974389aea40747220501&q=76504&days=10&aqi=no&alerts=no';
                await fetch(weatherApi)
                     .then((data) => data.json())
                     .then((res) => {
@@ -19,35 +19,44 @@ function Weather(props) {
 
      console.log(weatherData);
 
-     const displayCurrent = () => {
-          if (weatherData) {
+     const getForecast = () => {
+          const forecastData = weatherData.forecast.forecastday;
+          return forecastData.map((forecast, i) => {
                const options = { weekday: 'short' };
                const getDay = new Intl.DateTimeFormat('en-US', options);
-               const date_01 = new Date(
-                    `${weatherData.forecast.forecastday[0].date} 00:00`
+               const date = new Date(`${forecast.date} 00:00`);
+               const day = getDay.format(date);
+               console.log(forecast.date_epoch);
+               return (
+                    <div
+                         key={forecast.date_epoch}
+                         className='weather_forecast_day'
+                    >
+                         <h1>{day}</h1>
+                         <img src={forecast.day.condition.icon}></img>
+                         <h3>{`H ${forecast.day.maxtemp_f}°F`}</h3>
+                         <h3>{`L ${forecast.day.mintemp_f}°F`}</h3>
+                    </div>
                );
-               const date_02 = new Date(
-                    `${weatherData.forecast.forecastday[1].date} 00:00`
-               );
-               const date_03 = new Date(
-                    `${weatherData.forecast.forecastday[2].date} 00:00`
-               );
-               const day_01 = getDay.format(date_01);
-               const day_02 = getDay.format(date_02);
-               const day_03 = getDay.format(date_03);
-               console.log(day_01, day_02, day_03);
+          });
+     };
+
+     const displayCurrent = () => {
+          if (weatherData) {
                return (
                     <div className='weather_current'>
+                         <h1 className='weather_current_label'>{`${weatherData.location.name}, ${weatherData.location.region}`}</h1>
                          <h1 className='weather_current_label'>Current</h1>
                          <h1 className='weather_current_temp'>
                               {weatherData.current.temp_f}°F
                          </h1>
                          <img
                               className='weather_current_icon'
-                              width='75px'
-                              height='75px'
+                              width='40px'
+                              height='40px'
                               src={weatherData.current.condition.icon}
                          ></img>
+                         <div className='weather_forecast'>{getForecast()}</div>
                     </div>
                );
           }
