1a4dc1d63cdd819687945095a024dda505b718af
trevor <luper4@gmail.com>
2022-01-11 22:01:39 -0600

    Fixed login component, Added topbar and added dock components

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -3,15 +3,19 @@ import './weather.css';
 
 function Weather(props) {
      const [weatherData, setWeatherData] = useState();
+     const user = props.user;
+     const round = Math.round;
 
      useEffect(() => {
           const getData = async () => {
-               const weatherApi =
-                    'http://api.weatherapi.com/v1/forecast.json?key=25bd208b8a974389aea40747220501&q=76504&days=10&aqi=no&alerts=no';
+               const weatherApi = `https://api.weatherapi.com/v1/forecast.json?key=25bd208b8a974389aea40747220501&q=${user.zipCode}&days=3&aqi=no&alerts=no`;
                await fetch(weatherApi)
                     .then((data) => data.json())
                     .then((res) => {
                          setWeatherData(res);
+                    })
+                    .catch((err) => {
+                         console.log(err);
                     });
           };
           getData();
@@ -24,9 +28,11 @@ function Weather(props) {
           return forecastData.map((forecast, i) => {
                const options = { weekday: 'short' };
                const getDay = new Intl.DateTimeFormat('en-US', options);
-               const date = new Date(`${forecast.date} 00:00`);
+               const date = new Date(
+                    `${forecast.date.replace(/-/g, '/')} 00:00`
+               );
+               console.log(forecast.day.maxtemp_f);
                const day = getDay.format(date);
-               console.log(forecast.date_epoch);
                return (
                     <div
                          key={forecast.date_epoch}
@@ -34,8 +40,12 @@ function Weather(props) {
                     >
                          <h1>{day}</h1>
                          <img src={forecast.day.condition.icon}></img>
-                         <h3>{`H ${forecast.day.maxtemp_f}°F`}</h3>
-                         <h3>{`L ${forecast.day.mintemp_f}°F`}</h3>
+                         <h3>{`H ${round(forecast.day.maxtemp_f)}°F`}</h3>
+                         <h3>{`L ${round(forecast.day.mintemp_f)}°F`}</h3>
+                         <h3>{`% of Rain`}</h3>
+                         <h3>{`${round(
+                              forecast.day.daily_chance_of_rain
+                         )}%`}</h3>
                     </div>
                );
           });
@@ -48,7 +58,7 @@ function Weather(props) {
                          <h1 className='weather_current_label'>{`${weatherData.location.name}, ${weatherData.location.region}`}</h1>
                          <h1 className='weather_current_label'>Current</h1>
                          <h1 className='weather_current_temp'>
-                              {weatherData.current.temp_f}°F
+                              {round(weatherData.current.temp_f)}°F
                          </h1>
                          <img
                               className='weather_current_icon'
